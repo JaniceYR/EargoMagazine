eff80a3bc95eff5aa358833ed55814015ceb8d0f
Janice Yura Lee <janiceyuralee@gmail.com>
2017-07-03 11:33:55 -0700

    Add model and controller

---
+++ app/helpers/api/wiki_helper.rb
@@ -0,0 +1,2 @@
+module Api::WikiHelper
+end
---
+++ test/controllers/api/wiki_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::WikiControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
