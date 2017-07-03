1e207dfbda3340872868c124c87884ed25e0200b
Janice Yura Lee <janiceyuralee@gmail.com>
2017-07-03 11:56:33 -0700

    Add annotate

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: wikis
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  logo_url   :string
+#  site_type  :string
+#  editor     :string
+#  owner      :string
+#  url        :string
+#  founded    :integer
+#  created    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Wiki < ApplicationRecord
 end
--- test/models/wiki_test.rb
+++ test/models/wiki_test.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: wikis
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  logo_url   :string
+#  site_type  :string
+#  editor     :string
+#  owner      :string
+#  url        :string
+#  founded    :integer
+#  created    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class WikiTest < ActiveSupport::TestCase
