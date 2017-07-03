ca622fc69089a66a5e5b09fb746fb4b799bb4d01
Janice Yura Lee <janiceyuralee@gmail.com>
2017-07-03 11:55:01 -0700

    Add migration

--- db/migrate/20170703183342_create_wikis.rb
+++ db/migrate/20170703183342_create_wikis.rb
@@ -1,7 +1,16 @@
 class CreateWikis < ActiveRecord::Migration[5.0]
   def change
     create_table :wikis do |t|
+      t.string :name
+      t.string :logo_url
+      t.string :site_type
+      t.string :editor
+      t.string :owner
+      t.string :url
+      t.integer :founded
+      t.string :created
 
+      t.index :name
       t.timestamps
     end
   end
