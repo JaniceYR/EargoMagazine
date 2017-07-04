449f68708d3df2962f5775986ec193a2791010c1
Janice Yura Lee <janiceyuralee@gmail.com>
2017-07-03 20:27:29 -0700

    Add react skeleton

---
+++ frontend/root.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Root extends React.Component {
+  render() {
+    return(
+      <div>
+        reacT!!!!
+      </div>
+    );
+  }
+}
+
+export default Root;
