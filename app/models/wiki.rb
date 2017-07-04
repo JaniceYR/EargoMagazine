e034313146465cb892b83a002ebead444759ff9f
Janice Yura Lee <janiceyuralee@gmail.com>
2017-07-03 19:56:40 -0700

    Delete useless comments

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -35,73 +35,53 @@ class Wiki < ApplicationRecord
         case ele
         when /\A\| title/
           wiki.name = ele.split("=")[1].strip.delete("[]{}'")
-          p wiki.name
         when /\A\|title/
           wiki.name = ele.split("=")[1].strip.delete("[]{}'")
-          p wiki.name
         when /\A\| name/
           wiki.name = ele.split("=")[1].strip.delete("[]{}'")
-          p wiki.name
         when /\A\| logo /
           wiki.logo_url = ele.split("=")[1].strip.delete("[]{}'").split("|")[0]
           wiki.logo_url.slice!("File:")
-          p wiki.logo_url
         when /\A\|logo /
           wiki.logo_url = ele.split("=")[1].strip.delete("[]{}'").split("|")[0]
           wiki.logo_url.slice!("File:")
-          p wiki.logo_url
         when /\A\|image_file /
           wiki.logo_url = ele.split("=")[1].strip.delete("[]{}'").split("|")[0]
           wiki.logo_url.slice!("File:")
-          p wiki.logo_url
         when /\A\| type/
           wiki.site_type = ele.split("=")[1].strip.delete("[]")
-          p wiki.site_type
         when /\A\|type/
           wiki.site_type = ele.split("=")[1].strip.delete("[]")
-          p wiki.site_type
         when /\A\| editor/
           wiki.editor = ele.split("=")[1].strip.delete("[]")
-          p wiki.editor
         when /\A\|editor/
           wiki.editor = ele.split("=")[1].strip.delete("[]")
-          p wiki.editor
         when /\A\| owner/
           wiki.owner = ele.split("=")[1].strip.delete("[]").split("<br>").join(", ")
-          p wiki.owner
         when /\A\| url/
           wiki.url = ele.split("=")[1].strip.delete("[]{}'")
           wiki.url.slice!("URL|")
-          p wiki.url
         when /\A\| website/
           wiki.url = ele.split("=")[1].strip.delete("[]{}'").split("|")[1]
-          p wiki.url
         when /\A\|website/
           wiki.url = ele.split("=")[1].strip.delete("[]{}'").split("|")[1]
-          p wiki.url
         when /\A\| founded/
           wiki.founded = ele.split("=")[1].split("{{start date")[1].split("|")[1]
-          p wiki.founded
         when /\A\| launch date/
           wiki.founded = ele.split("=")[1].split("{{start date")[1].split("|")[1]
           if wiki.founded.zero?
             wiki.founded = ele.split("{{start date")[1].split("|")[2]
           end
-          p wiki.founded
         when /\A\| launch_date/
           wiki.founded = ele.split("=")[1].split("{{start date")[1].split("|")[1]
-          p wiki.founded
         when /\A\|firstdate/
           wiki.founded = ele.split("=")[1].split("{{start date")[1].split("|")[1]
-          p wiki.founded
         when /\A\| author/
           wiki.created = ele.split("=")[1].strip.delete("[]{}'").split("<br>").join(", ")
-          p wiki.created
         end
       end
       wikis << wiki
     end
     wikis
-
   end
 end
